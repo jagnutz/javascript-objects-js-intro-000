6a9c6c723138f6cccc29305297a0f3a661fb6518
Michael <chaphlux@yahoo.com>
2018-12-29 23:28:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = {artistName : "songTitle"};
 
 function updatePlaylist (playlist, artistName, songTitle) {
-    playlist[artistnames] = 'song title'
+    playlist[artistName] = songTitle
 
     return playlist
 }
-
