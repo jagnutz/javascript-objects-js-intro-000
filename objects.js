543a0da872de7f280f66ba46be06114802d44fc4
Michael <chaphlux@yahoo.com>
2018-12-29 23:33:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  Delete playlist.artistName
+  delete playlist.artistName
 
   return playlist
 }
